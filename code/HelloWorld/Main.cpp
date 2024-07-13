c4ce46508980b3738862a222c4d2b25c2d5c2ec2
qqssdda <sj1050437739@gmail.com>
2024-07-13 18:26:47 +0800

    条件分支与循环

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -3,6 +3,41 @@
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	Log("Hello, World!");	// << 的本质是一个函数,将字符串推送到cout流中
+	if (4 > 3)
+		Log("true");
+
+	for (int i = 0; i < 10; i++)
+	{
+		Log("Hello World!");
+	}
+
+	unsigned int i = 0;
+
+	for (; i < 10;)
+	{
+		Log("Hello World!");
+		i++;
+	}
+
+	/*for (;;)
+	{
+		Log("死循环");
+	}*/
+
+	unsigned g = i;
+
+	while (i < 5)
+	{
+		Log("while循环!");
+	}
+
+	unsigned int j = 0;
+
+	do 
+	{
+		Log("先执行一遍");
+		j++;
+	} while (j < 5);
+
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
