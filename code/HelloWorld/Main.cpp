0142be7f96ae9a97b045bb8f906afaa087016f59
qqssdda <sj1050437739@gmail.com>
2024-07-13 17:27:04 +0800

    C++函数

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
 
-void Log(const char* message);
+int Multiply(int a, int b)
+{
+	return a * b;
+}
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	unsigned int variable = 8;
+	int result = Multiply(3, 5);
 
-	std::cout << variable << std::endl;	// << 的本质是一个函数,将字符串推送到cout流中
+	std::cout << "Hello World!" << std::endl;	// << 的本质是一个函数,将字符串推送到cout流中
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
