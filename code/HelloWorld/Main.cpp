068e3bcb70143d9f2f2962801b3213991c254fa5
qqssdda <sj1050437739@gmail.com>
2024-07-22 14:52:10 +0800

    C++指针

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,43 +1,11 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
-#include "Log.h"
+#define LOG(x) std::cout << x << std::endl
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	if (4 > 3)
-		Log("true");
-
-	for (int i = 0; i < 10; i++)
-	{
-		Log("Hello World!");
-	}
-
-	unsigned int i = 0;
-
-	for (; i < 10;)
-	{
-		Log("Hello World!");
-		i++;
-	}
-
-	/*for (;;)
-	{
-		Log("死循环");
-	}*/
-
-	unsigned g = i;
-
-	while (i < 5)
-	{
-		Log("while循环!");
-	}
-
-	unsigned int j = 0;
-
-	do 
-	{
-		Log("先执行一遍");
-		j++;
-	} while (j < 5);
+	int var = 8;
+	int* ptr = &var;
+	*ptr = 10;
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
