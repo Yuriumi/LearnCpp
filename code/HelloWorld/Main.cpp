2d5b63dde9a5cd59be76df71f482b4f4f79603de
qqssdda <sj1050437739@gmail.com>
2024-07-22 17:00:20 +0800

    C++结构体

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -20,6 +20,16 @@ void Move(Player& player,int xa, int ya)
 	player.y += ya * player.speed;
 }
 
+struct Vec2
+{
+	float x, y;
+
+	void Add(Vec2& vec2) {
+		x + vec2.x;
+		y + vec2.y;
+	}
+};
+
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
 	Player player;
