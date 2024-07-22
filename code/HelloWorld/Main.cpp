2987bc617c6f982ac027f8afe6087ff5c5770d7f
qqssdda <sj1050437739@gmail.com>
2024-07-22 16:23:31 +0800

    C++引用

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
 #define LOG(x) std::cout << x << std::endl
 
+void Increment(int& value) 
+{
+	value++;
+}
+
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	int var = 8;
-	int* ptr = &var;
-	*ptr = 10;
+	int a = 8;
+	int& ref = a;
+	LOG(a);
+
+	Increment(a);
+	LOG(a);
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
