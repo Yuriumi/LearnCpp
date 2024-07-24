b20c4d04594dabba65fa7a268574100145f3505f
qqssdda <sj1050437739@gmail.com>
2024-07-24 20:58:38 +0800

    C++接口(纯虚函数)

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -2,12 +2,22 @@
 #include <string>
 #include "Log.cpp"
 
-class Entity
+class Printable
+{
+public:
+	virtual std::string GetClassName() = 0;
+};
+
+class Entity : public Printable
 {
 public:
 	virtual std::string GetName()
 	{
 		return "Cherno";
+	}
+
+	std::string GetClassName() {
+		return "Entity";
 	}
 };
 
@@ -20,11 +30,20 @@ public:
 	{
 		return "Player";
 	}
+
+	std::string GetClassName() {
+		return "Player";
+	}
 };
 
 void PrintName(Entity* e)
 {
 	LOG(e->GetName());
+}
+
+void PrintClassName(Printable* p)
+{
+	LOG(p->GetClassName());
 }
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
@@ -33,6 +52,9 @@ int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 	PrintName(e);
 	Player* player = new Player();
 	PrintName(player);
+
+	PrintClassName(e);
+	PrintClassName(player);
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
