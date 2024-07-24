73f1328eabfd845c30270269d63d576ba3d8dc5d
qqssdda <sj1050437739@gmail.com>
2024-07-24 16:53:23 +0800

    C++虚函数

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
+#include <string>
 #include "Log.cpp"
 
 class Entity
 {
 public:
-	float x{ 0 }, y{ 0 };
-
-	void Move(float xa, float ya)
+	virtual std::string GetName()
 	{
-		LOG(x + xa << y + ya);
+		return "Cherno";
 	}
 };
 
@@ -17,16 +16,23 @@ class Player : public Entity
 public:
 	const char* name;
 
-	void PrintName()
+	std::string GetName()
 	{
-		LOG(name);
+		return "Player";
 	}
 };
 
+void PrintName(Entity* e)
+{
+	LOG(e->GetName());
+}
+
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	Player player;
-	player.Move(2, 3);
+	Entity* e = new Entity();
+	PrintName(e);
+	Player* player = new Player();
+	PrintName(player);
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
