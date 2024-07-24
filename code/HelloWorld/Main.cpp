2582f78f95e7798c820c8e3b4f53d6739d848464
qqssdda <sj1050437739@gmail.com>
2024-07-24 16:16:38 +0800

    C++构造与析构函数

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,41 +1,49 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
 #include "Log.cpp"
 
-void function()
-{
-	static int count{ 0 };
-	count++;
-	LOG(count);
-}
-
-class Singeton
-{
-private:
-	static Singeton* instance;
-public :
-	static Singeton& Get()
-	{
-		return *instance;
-	}
-};
-
 class Entity
 {
 public:
-	static Singeton& Get()
+	int X, Y;
+
+	Entity(int x, int y)
 	{
-		static Singeton instance;
-		return instance;
+		X = x;
+		Y = y;
+	}
+
+	void Print()
+	{
+		LOG(X << ',' << Y);
+	}
+
+	~Entity()
+	{
+		LOG("Delte!");
+	}
+
+};
+
+class M_LOG
+{
+public:
+	M_LOG() = delete;
+
+	static void Write()
+	{
+
 	}
 };
 
+void Function()
+{
+	Entity e(10, 5);
+	e.Print();
+}
+
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	Log log;
-	log.SetLevel(Log::Info);
-	log.LogInfo("error!");
-	log.LogWarn("error!");
-	log.LogError("error!");
+	Function();
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
