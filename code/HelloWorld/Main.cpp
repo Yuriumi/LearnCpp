0621992688dc13d4bbe00d390259c31248c80e9a
qqssdda <sj1050437739@gmail.com>
2024-07-26 10:04:12 +0800

    explicit关键字

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -2,36 +2,20 @@
 #include <string>
 #include "Log.cpp"
 
-class Entity 
+class Entity
 {
-	std::string m_name;
+public:
+	std::string m_Name;
+	int m_Age;
+	bool m_Attack;
+
+	explicit Entity(bool attack)
+		: m_Name("Unknow"), m_Age(-1), m_Attack(0) {}
 };
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
-	int* a = new int;
-	int* b = new int[50];
-
-	delete a;
-	delete[] b;
-
-	Entity* e1 = new Entity;
-	Entity* e2 = new Entity[50];
-
-	delete e1;
-	delete[] e2;
-
-	// 分配内存
-	Entity* e = new Entity;
-	Entity* e3 = (Entity*)malloc(sizeof(Entity));
-	delete e;
-	delete e3;
-
-	int* d = new int[50];
-	Entity* e4 = new(d) Entity;
-
-	delete e4;
-	delete d;
+	Entity e = "1111";
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
