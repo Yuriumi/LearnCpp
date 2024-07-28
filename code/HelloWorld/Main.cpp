16c700fb45a36fb42c9989d013b12399c0539500
qqssdda <sj1050437739@gmail.com>
2024-07-28 13:33:56 +0800

    补充README，智能指针

--- code/HelloWorld/Main.cpp
+++ code/HelloWorld/Main.cpp
@@ -1,100 +1,42 @@
 #include <iostream>	// 预处理，在实际编译之前就被处理了
 #include <string>
+#include <memory>
 #include "Log.cpp"
 
-struct Vector2
-{
-	float x, y;
-
-	Vector2(float x, float y)
-		:x(x),y(y){}
-
-	Vector2 Add(const Vector2& other) const
-	{
-		return Vector2(x + other.x, y + other.y);
-	}
-
-	Vector2 operator+(const Vector2& other) const
-	{
-		return Add(other);
-	}
-
-	Vector2 Multipy(const Vector2& other) const
-	{
-		return Vector2(x * other.x, y * other.y);
-	} 
-
-	Vector2 operator*(const Vector2& other) const
-	{
-		return Multipy(other);
-	}
-
-	bool operator==(const Vector2& other)
-	{
-		return x == other.x && y == other.y;
-	}
-
-	bool operator!=(const Vector2& other)
-	{
-		return !(*this == other);
-	}
-};
-
-std::ostream& operator<<(std::ostream& stream, const Vector2& other)
-{
-	stream << other.x << other.y;
-	return stream;
-}
-
-class Example
+class Entity
 {
 private:
-	int x, y;
-
+	const std::string m_Name;
 public:
-	Example(int x, int y)
+	Entity(const std::string name)
+		:m_Name("Know") {}
+
+	void PrintName()
 	{
-		this->x = x;
-		this->y = y;
-
-		Example* e = this;
-		e->x = x;
-	}
-
-	int GetX() const
-	{
-		const Example* e = this;
-		return e->x;
-	}
-
-	void PrintExample()
-	{
-		const Example& e = *this;
-		std::cout << e.GetX() << std::endl;
-	}
-};
-
-class ScopPtr
-{
-private:
-	Example* m_Ptr;
-
-public:
-	ScopPtr(Example* ptr)
-		:m_Ptr(ptr){}
-
-	~ScopPtr()
-	{
-		LOG(123);
-		delete m_Ptr;
+		LOG(m_Name);
 	}
 };
 
 int main()	// main函数是程序的入口,main函数的返回值是特殊的，在默认情况下返回0
 {
 	{
-		ScopPtr example(new Example(1,1));
+		std::unique_ptr<Entity> e = std::make_unique<Entity>();
+		e->PrintName();
+
+		//std::unique_ptr<Entity> e1 = e;
 	}
+	// e->PrintName();
+
+	std::shared_ptr<Entity> shareE;
+	std::weak_ptr<Entity> weakE;
+	{
+		std::shared_ptr<Entity> e = std::make_shared<Entity>();
+		shareE = e;
+		weakE = e;
+	}
+
+	//weakE->PrintName();
+
 
 	std::cin.get();	// cin/cout 不是函数,是输入输出流对象
 }
